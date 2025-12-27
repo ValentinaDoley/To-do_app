82da09081dd35c4a0b6b9258041612f5fb52e5ec
Valentina D Luffy <valentinadoley10@gmail.com>
2025-12-28 02:22:22 +0530

    Initial stage of app makinng

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// step 1 object structure for a single task
 const task = {
     id: 'task-1',
     title: 'Work on my portfolio.',
@@ -5,3 +6,10 @@ const task = {
     createdAt: new Date(),
     dueAt: null // for reminders
 }
+// step 2 to store the tasks in local storage
+ function getTasks(){
+    const stored = localStorage.getItem('tasks');
+    if{
+        stored() = null;
+    }
+ }
